0637c0787ed619e42b73ee6fb9084ed013d807e9
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-08-22 01:26:53 -0300

    feat: implement max number of tasks validation

--- src/services/TasksService.ts
+++ src/services/TasksService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository, Repository } from 'typeorm';
 import Task from '../models/Task';
 import TasksRepository from '../repositories/TasksRepository';
+import * as Yup from 'yup';
 
 interface ITasksCreate {
   item: string;
@@ -18,9 +19,29 @@ class TasksService {
     this.tasksRepository = getCustomRepository(TasksRepository);
   }
   async create({ item }: ITasksCreate) {
+    const tasks = await this.tasksRepository.find();
+    const maxNumberOfTasks = tasks.length >= 100;
+
+    if (maxNumberOfTasks) {
+      throw new Error('Limit of 100 tasks reached!');
+    }
+
     const task = this.tasksRepository.create({
       item,
     });
+
+    try {
+      const schema = Yup.object().shape({
+        item: Yup.string()
+          .required("Please enter the required field")
+          .matches(/^([A-Za-z1234567890\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+            'Cannot contain special characters.')
+      })
+
+      await schema.validate(task, { abortEarly: false });
+    } catch (err) {
+      throw new Error('Cannot contain special characters.')
+    }
 
     await this.tasksRepository.save(task);
 
