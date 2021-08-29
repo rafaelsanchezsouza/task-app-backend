7a9df348df8e8d35c95539cbbefb38725b9061b8
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-08-29 20:21:57 -0300

    build: use environmental variables

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+const srcConfig = {
+  "type": "mongodb",
+  "url": `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.anpcq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  "useNewUrlParser": true,
+  "synchronize": true,
+  "logging": true,
+  "entities": [
+    "src/entity/**/*.ts"
+  ],
+  "useUnifiedTopology": true
+}
+module.exports = srcConfig;
